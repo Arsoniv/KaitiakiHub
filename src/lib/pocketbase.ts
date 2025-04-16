1284106e9ab18e4d24912d4b3dc4828e97edf2bf
qwesome <orsonhitchman@gmail.com>
2025-04-17 00:24:04 +1200

    Remove all the unneeded new lines & probably break half the code when trying to remove comments.

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,11 +1,1 @@
-import PocketBase from "pocketbase";
-import { writable } from 'svelte/store';
-
-export const pb = new PocketBase("https://khubbackend.duckdns.org"); // need to use subdomain so that https would work
-pb.autoCancellation(false);
-
-export const authData = writable(pb.authStore.record);
-
-pb.authStore.onChange(() => {
-    authData.set(pb.authStore.record);
-});
+import PocketBase from "pocketbase";import { writable } from 'svelte/store';export const pb = new PocketBase("https://khubbackend.duckdns.org"); .autoCancellation(false);export const authData = writable(pb.authStore.record);pb.authStore.onChange(() => {    authData.set(pb.authStore.record);});
